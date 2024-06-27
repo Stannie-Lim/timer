e33f4ccaca6a9d17535620ad3dfe530819a7c714
Stanley Lim <stannielim@meta.com>
2024-06-27 12:31:24 -0400

    fake request

--- frontend/src/Background.jsx
+++ frontend/src/Background.jsx
@@ -1,9 +1,17 @@
 import { Paper } from '@mui/material';
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import hene from './assets/hene.png';
 
 export const Background = ({ children }) => {
+  useEffect(() => {
+    const fakeRequest = async () => {
+      await axios.get(`${import.meta.env.VITE_APP_BACKEND_URL}`);
+    }
+
+    fakeRequest();
+  }, []);
+
   return (
     <div style={{ backgroundSize: 'cover', backgroundImage: `url("${hene}")`, height: '100vh', width: '100vw', display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: "column" }}>
       <img src="https://i.mapleranks.com/u/IBKGPJKPNIJOCNMDENBMPBIFHHDDKOKELENJCCNEBBHADNBIFMKNHAENIBHLFHDDCOMNDBDNNEEMNNAGJLFLKJLDFALEMAEBBPCGJMGIALHGPDFBALMKGELKFFKKDGKOENCFEMFDPGNBMBHKEAMNAAKIGFJMJGJGDHCPCNJMFIEPAPCCKIHNAJPEEDEGPGGFEKLIEAADHBDBLMIEBGCIJGBAOLCMIOBDBHOPFBOMNELCHMDPHHIDHMJEMFDLMFPH.png" height={150} style={{ overflow: 'visible', zIndex: 1, marginTop: -150 }} />
